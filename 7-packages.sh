8a90683c84111d4c9a3f50197e183b37fa671ef9
Jérémy Turazzi <git@jtrz.fr>
2023-05-17 15:31:53 +0200

    🐛 FIX: packages.sh

--- 7-packages.sh
+++ 7-packages.sh
@@ -4,7 +4,7 @@
 yay -S mkinitcpio-firmware sof-firmware
 
 # KDE
-yay -S qt6-wayland xdg-desktop-portal xdg-desktop-portal-kde ffmpegthumbs lib32-pipewire octopi octopi-notifier-qt5 p7zip power-profiles-daemon spectacle  unrar
+yay -S qt6-wayland xdg-desktop-portal xdg-desktop-portal-kde ffmpegthumbs lib32-pipewire octopi octopi-notifier-qt5 p7zip power-profiles-daemon spectacle unrar
 
 # FONTS
 yay -S libunity noto-fonts noto-fonts-emoji ttf-liberation ttf-meslo-nerd-font-powerlevel10k
